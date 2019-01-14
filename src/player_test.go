bf3e27784b9f03f3d0eec87f91d2d72ac054ecb9
HakanSunay <hakansunay@abv.bg>
2019-01-15 00:37:00 +0200

    Add tests for player and room

---
+++ src/player_test.go
@@ -0,0 +1,51 @@
+package src
+
+import (
+	"testing"
+)
+
+func TestPlayer_Die(t *testing.T) {
+	player := Player{}
+
+	player.Die()
+
+	if player.Dead == false {
+		t.Error("Player didn't die, Die not working!")
+	}
+}
+
+func TestPlayer_Save(t *testing.T) {
+	player := Player{}
+
+	player.Save()
+
+	if player.Invulnerable == false {
+		t.Error("Player wasn't saved, Save not working!")
+	}
+}
+
+func TestPlayer_Blame(t *testing.T) {
+	player1 := Player{}
+	player2 := Player{}
+
+	player1.Blame(&player2)
+
+	if player2.Votes == 0 {
+		t.Error("Vote not counted, Blame not working!")
+	}
+}
+
+func TestPlayer_ResetRound(t *testing.T) {
+	players := [6]Player{}
+	for _, pl := range players{
+		pl.Votes++
+	}
+	for _, pl := range players{
+		pl.ResetRound()
+		if pl.Votes != 0 {
+			t.Error("Reset round not working!")
+		}
+	}
+
+
+}
---
+++ src/room_test.go
@@ -0,0 +1,49 @@
+package src
+
+import "testing"
+
+func TestCreateRoom(t *testing.T) {
+	player := Player{}
+	player.Name = "Hakan"
+	room := CreateRoom("GoRoom", &player)
+	for _,pl := range room.players{
+		if pl.RoomOwner == true{
+			if pl.Name != player.Name{
+				t.Error("Room owner names don't match, CreateRoom not working!")
+			}
+		}
+	}
+}
+
+func TestRoom_AddPlayer(t *testing.T) {
+	room := Room{}
+	player := Player{}
+	room.AddPlayer(&player)
+	if len(room.players) == 0 {
+		t.Error("Empty room, AddPlayer not working!")
+	}
+}
+
+func TestRoom_AddPlayerBecomesRoomOwner(t *testing.T) {
+	room := Room{}
+	player := Player{}
+	room.AddPlayer(&player)
+	if room.players[0].RoomOwner == false {
+		t.Error("Room has no owner, AddPlayer not working!")
+	}
+}
+
+func TestRoom_Reset(t *testing.T) {
+	room := Room{}
+	players := [6]Player{}
+	for _, pl := range players{
+		pl.Invulnerable = true
+		room.AddPlayer(&pl)
+	}
+	room.Reset()
+	for _, pl := range players{
+		if pl.Invulnerable == true{
+			t.Error("Reset not working!")
+		}
+	}
+}
