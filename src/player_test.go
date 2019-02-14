d50841ad3b1226deb1b6a146d181e8d0577fef2d
HakanSunay <hakansunay@abv.bg>
2019-02-14 17:15:17 +0200

    Complete Player Tests

--- src/player_test.go
+++ src/player_test.go
@@ -15,23 +15,10 @@ func TestPlayer_Die(t *testing.T) {
 }
 
 func TestPlayer_Save(t *testing.T) {
-	player := Player{}
-
+	player := &Player{}
 	player.Save()
-
-	if player.Chosen == false {
+	if player.Chosen == true {
 		t.Error("Player wasn't saved, Save not working!")
-	}
-}
-
-func TestPlayer_Blame(t *testing.T) {
-	player1 := Player{}
-	player2 := Player{}
-
-	player1.CastVote(player2.Name)
-
-	if player2.Votes == 0 {
-		t.Error("Vote not counted, Blame not working!")
 	}
 }
 
@@ -63,29 +50,52 @@ func TestPlayer_CastVote(t *testing.T) {
 }
 
 func TestPlayer_AssignChosen(t *testing.T) {
-
-}
-
-func TestPlayer_CreateRoom(t *testing.T) {
-
+	coolPlayer := &Player{}
+	if coolPlayer.AssignChosen(); !(coolPlayer.Chosen){
+		t.Error("AssignChosen doesn't work!")
+	}
 }
 
 func TestPlayer_End(t *testing.T) {
-
+	coolPlayer := &Player{}
+	coolPlayer.Job = DOCTOR
+	if coolPlayer.End(); coolPlayer.Job != CITIZEN{
+		t.Error("End for player doesn't work!")
+	}
 }
 
 func TestPlayer_IncrementVote(t *testing.T) {
-
+	coolPlayer := &Player{}
+	if coolPlayer.IncrementVote(); coolPlayer.Votes != 1{
+		t.Error("IncrementVote for player doesn't work!")
+	}
 }
 
 func TestPlayer_IsEligibleToChat(t *testing.T) {
-
+	coolPlayer := &Player{}
+	coolRoom := &Room{}
+	coolPlayer.Room = coolRoom
+	if coolPlayer.Job = MAFIA; !(coolPlayer.IsEligibleToChat()){
+		t.Error("Is eligible to chat doesn't work!")
+	}
 }
 
 func TestPlayer_SetVotes(t *testing.T) {
-
+	coolPlayer := &Player{}
+	if coolPlayer.SetVotes(10); coolPlayer.Votes != 10 {
+		t.Error("SetVotes doesn't work!")
+	}
 }
 
 func TestPlayer_StartGame(t *testing.T) {
-
+	coolPlayer := &Player{}
+	coolRoom := Room{}
+	players := [6]Player{}
+	coolRoom.AddPlayer(coolPlayer)
+	for index, _ := range players {
+		coolRoom.AddPlayer(&players[index])
+	}
+	if !(coolPlayer.StartGame()){
+		t.Error("StartGame for player as room owner doesn't work!")
+	}
 }
