2a306b092dfe5d3b8192a02d82c73eccb33d213d
HakanSunay <hakansunay@abv.bg>
2019-02-14 17:03:16 +0200

    Add Player Test Skeletons

--- src/player_test.go
+++ src/player_test.go
@@ -19,7 +19,7 @@ func TestPlayer_Save(t *testing.T) {
 
 	player.Save()
 
-	if player.Invulnerable == false {
+	if player.Chosen == false {
 		t.Error("Player wasn't saved, Save not working!")
 	}
 }
@@ -28,7 +28,7 @@ func TestPlayer_Blame(t *testing.T) {
 	player1 := Player{}
 	player2 := Player{}
 
-	player1.Blame(&player2)
+	player1.CastVote(player2.Name)
 
 	if player2.Votes == 0 {
 		t.Error("Vote not counted, Blame not working!")
@@ -60,4 +60,32 @@ func TestPlayer_CastVote(t *testing.T) {
 	if votedPl.Votes != 1 {
 		t.Error("Cast vote not working!")
 	}
+}
+
+func TestPlayer_AssignChosen(t *testing.T) {
+
+}
+
+func TestPlayer_CreateRoom(t *testing.T) {
+
+}
+
+func TestPlayer_End(t *testing.T) {
+
+}
+
+func TestPlayer_IncrementVote(t *testing.T) {
+
+}
+
+func TestPlayer_IsEligibleToChat(t *testing.T) {
+
+}
+
+func TestPlayer_SetVotes(t *testing.T) {
+
+}
+
+func TestPlayer_StartGame(t *testing.T) {
+
 }
